9fb886af0dbc498f9c29e9240ef84c093fc61f07
ReidoII <cloudy778@hotmail.com>
2014-04-18 13:55:43 +1200

    adjustment

--- CageFight/gen/co/nz/splashYay/cagefight/R.java
+++ CageFight/gen/co/nz/splashYay/cagefight/R.java
@@ -22,40 +22,15 @@ public final class R {
         public static final int activity_vertical_margin=0x7f040001;
     }
     public static final class drawable {
-        public static final int button_server=0x7f020000;
-        public static final int button_server_xml=0x7f020001;
-        public static final int button_start=0x7f020002;
-        public static final int button_start_default=0x7f020003;
-        public static final int button_start_focused=0x7f020004;
-        public static final int button_start_pressed=0x7f020005;
-        public static final int client_button=0x7f020006;
-        public static final int client_button_pressed=0x7f020007;
-        public static final int client_button_xml=0x7f020008;
-        public static final int exit_button=0x7f020009;
-        public static final int exit_button_pressed=0x7f02000a;
-        public static final int exit_button_xml=0x7f02000b;
-        public static final int ic_launcher=0x7f02000c;
-        public static final int server_button=0x7f02000d;
-        public static final int server_button_pressed=0x7f02000e;
-        public static final int server_button_xml=0x7f02000f;
+        public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int IPAddress=0x7f080002;
-        public static final int RelativeLayout1=0x7f080001;
-        public static final int action_settings=0x7f080008;
-        public static final int btnClient=0x7f080006;
-        public static final int btnServer=0x7f080005;
-        public static final int button1=0x7f080003;
-        public static final int container=0x7f080004;
-        public static final int exit=0x7f080007;
-        public static final int textView1=0x7f080000;
+        public static final int action_settings=0x7f080001;
+        public static final int container=0x7f080000;
     }
     public static final class layout {
-        public static final int acitvity_splash=0x7f030000;
-        public static final int activity_client=0x7f030001;
-        public static final int activity_main=0x7f030002;
-        public static final int activity_menu=0x7f030003;
-        public static final int fragment_main=0x7f030004;
+        public static final int activity_main=0x7f030000;
+        public static final int fragment_main=0x7f030001;
     }
     public static final class menu {
         public static final int main=0x7f070000;
