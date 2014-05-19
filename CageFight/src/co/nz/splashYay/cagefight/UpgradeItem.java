21a91f944e5882a428be719fc0e9de09c24983f3
ReidoII <cloudy778@hotmail.com>
2014-05-20 11:36:34 +1200

    Missing class added

---
+++ CageFight/src/co/nz/splashYay/cagefight/UpgradeItem.java
@@ -0,0 +1,52 @@
+package co.nz.splashYay.cagefight;
+
+public class UpgradeItem {
+	
+	
+	private int damage;
+	private int health;
+	private int speed;
+	private int cooldown;
+	
+	private int creepDamage;
+	private int creepHealth;
+	private int creepSpeed;
+	private int creepCooldown;
+	private int amountOfCreeps;
+	
+	private int baseDamage;
+	private int baseHealth;	
+	private int baseCooldown;
+	
+	private int towerDamage;
+	private int towerHealth;	
+	private int towerCooldown;
+	public UpgradeItem(int damage, int health, int speed, int cooldown, int creepDamage, int creepHealth, int creepSpeed, int creepCooldown, int amountOfCreeps, int baseDamage, int baseHealth, int baseCooldown, int towerDamage, int towerHealth, int towerCooldown) {
+		super();
+		this.damage = damage;
+		this.health = health;
+		this.speed = speed;
+		this.cooldown = cooldown;
+		this.creepDamage = creepDamage;
+		this.creepHealth = creepHealth;
+		this.creepSpeed = creepSpeed;
+		this.creepCooldown = creepCooldown;
+		this.amountOfCreeps = amountOfCreeps;
+		this.baseDamage = baseDamage;
+		this.baseHealth = baseHealth;
+		this.baseCooldown = baseCooldown;
+		this.towerDamage = towerDamage;
+		this.towerHealth = towerHealth;
+		this.towerCooldown = towerCooldown;
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}
